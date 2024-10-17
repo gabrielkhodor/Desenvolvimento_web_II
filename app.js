c22d5ac7871aabe32f03117d83dd067faede59b6
gabrielkhodor <44448751+gabrielkhodor@users.noreply.github.com>
2024-10-17 19:37:01 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const app = express();
 
-const aluno = require('./routes/alunosRoutes');
-const index = require('./routes/indexRoutes');
+const livrosRoutes = require('./api/routes/livrosRoutes');
+const prefeituras = require('./api/routes/prefeiturasRoutes');
+const engenheiros = require('./api/routes/engenheirosRoutes');
+const index = require('./api/routes/indexRoutes');
 
 app.use(express.json())
 
+app.set('view engine', 'ejs');
+app.set("views",'./api/views')
+
 const port = 3000;
 
-app.use('/aluno', aluno);
+app.use('/livros', livrosRoutes);
+app.use('/prefeituras', prefeituras);
+app.use('/engenheiros', engenheiros);
 app.use("/", index);
 
-//servidor rodando
 app.listen(port, () => {
     console.log(`Aplicativo Rodando na Porta ${port}`);
 })
 
+const conexao = require("./api/config/conexao.js");
+
+module.exports = app;
